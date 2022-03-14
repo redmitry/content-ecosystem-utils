24f5807e458cb39e82d0e04c78008dfe9a8d964c
Dmitry Repchevsky <redmitry@list.ru>
2022-03-14 18:29:13 +0100

    Update import-changes.sh

--- scripts/git-utils/import-changes.sh
+++ scripts/git-utils/import-changes.sh
@@ -13,11 +13,17 @@ else
     lbl="$3"
 fi
 
+echo $1
+echo $2
+echo $3
+echo $lbl
+
 if git commit -m "$2 on $(date)"; then
   if $1; then
     git push
   else
     BRANCH_NAME=$lbl-$(date +%s%N | cut -b10-19)
+    echo $BRANCH_NAME
     git checkout -b $BRANCH_NAME
     git push --set-upstream origin $BRANCH_NAME
     hub pull-request -m "$2 on $(date)" -l $lbl
